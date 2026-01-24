fbde16038a6373a92d8bc7c7e6af79efe08ac83d
ann <anyu2021yang@gmail.com>
2026-01-23 22:33:06 -0500

    2026-01-23 edit. Think mac_u.v is correct. Added mac_u_tb.v but it's still empty. It will be the testbench file for mac_u.v later.

--- project_1/project_1.srcs/sources_1/new/mac_u.v
+++ project_1/project_1.srcs/sources_1/new/mac_u.v
@@ -30,18 +30,14 @@ module mac_u(
     output reg o_cout
     );
     
-    reg [27:0] p;    // p = a * b, 14 bits * 14 bits = 28 bits
-    reg [28:0] w_full_solution;   // one extra bit for carry bit
+    wire [27:0] p = i_a * i_b;    // p = a * b, 14 bits * 14 bits = 28 bits
+    wire [28:0] w_full_solution = {1'b0,p} + {15'b0,i_c};
     
     always @(negedge i_rst_n or posedge i_clk) begin    
         if (i_rst_n == 0) begin
             o_y <= 28'b0;
             o_cout <= 1'b0;
-            p <= 28'd0;
-            w_full_solution <= 29'd0;
         end else begin
-            p <= i_a * i_b;
-            w_full_solution = {1'b0,p} + {15'b0,i_c};
             o_y <= w_full_solution[27:0];
             o_cout <= w_full_solution[28];
         end
---
+++ project_1/project_1.srcs/sources_1/new/mac_u_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/23/2026 10:24:49 PM
+// Design Name: 
+// Module Name: mac_u_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mac_u_tb(
+
+    );
+endmodule
