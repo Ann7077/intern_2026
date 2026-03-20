f8d39308d72fdaf45e4cbf03d302e9b2c346c027
ann <anyu2021yang@gmail.com>
2026-03-20 15:31:11 -0400

    2026-03-20 edit 3. Part of mac_u was using using = instead of non-blocking <=, fined that. There is still a cycle issue that is waiting to be fixed.

--- project_1/project_1.srcs/sources_1/new/mac_u.v
+++ project_1/project_1.srcs/sources_1/new/mac_u.v
@@ -52,11 +52,11 @@ always @(posedge i_clk or negedge i_rst_n) begin
         sum = (mult >>> 7) + i_c;
 
         if (sum[8] != sum[7]) begin
-            o_y = sum[8] ? 8'b10000000 : 8'b01111111;   // saturated
+            o_y <= sum[8] ? 8'b10000000 : 8'b01111111;   // saturated
         end else begin
-            o_y = sum[7:0];   // no overflow, normal truncation
+            o_y <= sum[7:0];   // no overflow, normal truncation
         end
-        o_cout = (sum[8] != sum[7]);   // indicate truncation occured
+        o_cout <= (sum[8] != sum[7]);   // indicate truncation occured
     end
 end
 
