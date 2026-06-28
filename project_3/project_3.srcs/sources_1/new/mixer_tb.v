f947ba47766c9848733f5b4b4a3b8b2eae86c950
ann <anyu2021yang@gmail.com>
2026-06-28 13:20:05 -0700

    add a mixer_tb.v file, added the parameters and the input output signals

---
+++ project_3/project_3.srcs/sources_1/new/mixer_tb.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/28/2026 01:10:25 PM
+// Design Name: 
+// Module Name: mixer_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mixer_tb(
+    
+    parameter INPUT_W = 12;
+    parameter OUTPUT_W = 24;
+    
+    
+
+    );
+endmodule
