c0a711b0b5d231c5d60b49926317cabe0a4b8f84
ann <anyu2021yang@gmail.com>
2026-06-28 13:09:39 -0700

    make the bit width of input and output parameters

--- project_3/project_3.srcs/sources_1/new/mixer.v
+++ project_3/project_3.srcs/sources_1/new/mixer.v
@@ -20,18 +20,22 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module mixer (
+module mixer #(
+
+    parameter IN_W = 12,
+    parameter OUT_W = 24
+)(
 
     input  wire clk,
     input  wire rst_n,
 
-    input  wire signed [11:0] adc_data,   // single raw mixed signal from the ADC (12-bit signed)
+    input  wire signed [IN_W-1:0] adc_data,   // single raw mixed signal from the ADC (12-bit signed)
 
-    input  wire signed [11:0] dds_cos,    // cosine wave to extract the I-branch (12-bit signed) 
-    input  wire signed [11:0] dds_sin,    // sine wave to extract the Q-branch (12-bit signed) 
+    input  wire signed [IN_W-1:0] dds_cos,    // cosine wave to extract the I-branch (12-bit signed) 
+    input  wire signed [IN_W-1:0] dds_sin,    // sine wave to extract the Q-branch (12-bit signed) 
 
-    output reg  signed [23:0] i_out,      // separated In-Phase (I) baseband path data (24-bit signed)  
-    output reg  signed [23:0] q_out       // separated Quadrature (Q) baseband path data (24-bit signed) 
+    output reg  signed [OUT_W:0] i_out,      // separated In-Phase (I) baseband path data (24-bit signed)  
+    output reg  signed [OUT_W:0] q_out       // separated Quadrature (Q) baseband path data (24-bit signed) 
 );
   
     always @(negedge rst_n or posedge clk) begin
@@ -45,3 +49,4 @@ module mixer (
     end
 
 endmodule
+
